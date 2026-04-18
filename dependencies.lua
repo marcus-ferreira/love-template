2eba4ae1b24ed94297d88aba3d7186b0b54bb64f
Marcus Ferreira <mv.ferreirapinto@gmail.com>
2026-04-18 01:05:17 -0300

    Adds character dependency e remove collider

--- dependencies.lua
+++ dependencies.lua
@@ -6,10 +6,12 @@ require("src.libs.lua.table")
 -- Love
 animation = require("src.libs.love.animation")
 camera    = require("src.libs.love.camera")
-collider  = require("src.libs.love.collider")
+-- collider     = require("src.libs.love.collider")
 color     = require("src.libs.love.color")
 data      = require("src.libs.love.data")
 physics   = require("src.libs.love.physics")
 state     = require("src.libs.love.state")
 timer     = require("src.libs.love.timer")
 vector    = require("src.libs.love.vector")
+
+character = require("src.libs.love.character")
