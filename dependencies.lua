f4ea523935de92710eb527a63c868f35a24f46fd
Marcus Ferreira <mv.ferreirapinto@gmail.com>
2026-04-21 02:03:32 -0300

    Revert "Fixes shapes in physics and removes simplePhysics"
    This reverts commit 5ab8ed0960a49999514f1418e669d61de1027b00.

--- dependencies.lua
+++ dependencies.lua
@@ -2,18 +2,18 @@
 require("src.libs.lua.math")
 require("src.libs.lua.string")
 require("src.libs.lua.table")
-require("src.libs.lua.utils")
 
 -- Love
 animationManager = require("src.libs.love.animationManager")
 camera           = require("src.libs.love.camera")
+collision        = require("src.libs.love.collision")
 color            = require("src.libs.love.color")
 data             = require("src.libs.love.data")
 -- debugLove = require("src.libs.love.debug")
-physics          = require("src.libs.love.physics")
+-- physics      = require("src.libs.love.physics")
 stateManager     = require("src.libs.love.stateManager")
 timer            = require("src.libs.love.timer")
 vector           = require("src.libs.love.vector")
 
 -- Classes
--- entity           = require("src.libs.classes.entity")
+entity           = require("src.libs.classes.entity")
