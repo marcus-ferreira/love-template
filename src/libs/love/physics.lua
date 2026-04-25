fa55a21d2233213ac89822ad50325d247ed61075
Marcus Ferreira <mv.ferreirapinto@gmail.com>
2026-04-24 23:34:27 -0300

    Changes linearDamping

--- src/libs/love/physics.lua
+++ src/libs/love/physics.lua
@@ -107,7 +107,7 @@ function physics.newRectangleCollider(world, x, y, width, height, type)
     local shape = love.physics.newRectangleShape(width, height)
     local fixture = love.physics.newFixture(body, shape)
     body:setFixedRotation(true)
-    body:setLinearDamping(10)
+    body:setLinearDamping(20)
     fixture:setFriction(0)
 
 
