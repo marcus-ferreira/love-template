e497fca733a79060c2f0a73c295b37284b686f4a
Marcus Ferreira <mv.ferreirapinto@gmail.com>
2026-05-04 02:08:20 -0300

    Accepts new changes

--- main.lua
+++ main.lua
@@ -16,9 +16,9 @@ function love.load()
         ---@param coll love.Contact
         beginContact = function(a, b, coll)
             local userDatas = { a:getUserData(), b:getUserData() }
-            if table.contains(userDatas, "footSensor") then
+            if table.containsAll(userDatas, { "footSensor", "ground" }) then
                 player:getCollider():addContact("ground")
-            elseif table.contains(userDatas, "leftSensor") or table.contains(userDatas, "rightSensor") then
+            elseif table.containsAny(userDatas, { "leftSensor", "rightSensor" }) and table.contains(userDatas, "wall") then
                 player:getCollider():addContact("wall")
             end
         end,
@@ -27,57 +27,59 @@ function love.load()
         ---@param coll love.Contact
         endContact = function(a, b, coll)
             local userDatas = { a:getUserData(), b:getUserData() }
-            if table.contains(userDatas, "footSensor") then
+            if table.containsAll(userDatas, { "footSensor", "ground" }) then
                 player:getCollider():removeContact("ground")
-            elseif table.contains(userDatas, "leftSensor") or table.contains(userDatas, "rightSensor") then
+            elseif table.containsAny(userDatas, { "leftSensor", "rightSensor" }) and table.contains(userDatas, "wall") then
                 player:getCollider():removeContact("wall")
             end
         end
     })
 
-    player = entity.newPlatformerEntity(world, 100, 100, 100, 300)
-    player:getCollider():addFixtures({
-        ["main"] = { "polygon", false, 0, 0, 24, 16 },
-        ["footSensor"] = { "polygon", true, 0, 8, 18, 4 },
-        ["leftSensor"] = { "polygon", true, -12, 0, 4, 10 },
-        ["rightSensor"] = { "polygon", true, 12, 0, 4, 10 }
-    })
-    player:getAnimationManager():addAnimations({
-        ["idle"] = { assets.images.player:getImage(), assets.images.player:getGrid(1), { 1 }, 16, 24 },
-        ["attack"] = { assets.images.player:getImage(), assets.images.player:getGrid(2), { 1, 2, 3, 4, 5 }, 16, 32, 0.07 }
-    })
-    player:getStateManager():addStates({
-        ["idle"] = {
-            enter = function()
-                player:getAnimationManager():changeAnimation("idle")
-            end,
-            update = function()
-                if input.isActionPressed("attack") then
-                    player:getStateManager():changeState("attack")
-                end
-            end
+    player = entity.newPlatformerEntity(world, 100, 100, 100, 300, {
+        fixtures = {
+            ["main"] = { "polygon", false, 0, 0, 24, 16 },
+            ["footSensor"] = { "polygon", true, 0, 8, 18, 4 },
+            ["leftSensor"] = { "polygon", true, -12, 0, 4, 10 },
+            ["rightSensor"] = { "polygon", true, 12, 0, 4, 10 }
         },
-        ["attack"] = {
-            enter = function()
-                player:getAnimationManager():changeAnimation("attack")
-            end,
-            update = function()
-                if player:getAnimationManager():getCurrentAnimation():isEnded() then
-                    player:getStateManager():changeState("idle")
+        animations = {
+            ["idle"] = { assets.images.player:getImage(), assets.images.player:getGrid(1), { 1 }, 16, 24 },
+            ["attack"] = { assets.images.player:getImage(), assets.images.player:getGrid(2), { 1, 2, 3, 4, 5 }, 16, 32, 0.07 },
+        },
+        states = {
+            ["idle"] = {
+                enter = function()
+                    player:getAnimationManager():changeAnimation("idle")
+                end,
+                update = function()
+                    if input.isActionPressed("attack") then
+                        player:getStateManager():changeState("attack")
+                    end
                 end
-            end
+            },
+            ["attack"] = {
+                enter = function()
+                    player:getAnimationManager():changeAnimation("attack")
+                end,
+                update = function()
+                    if player:getAnimationManager():getCurrentAnimation():isEnded() then
+                        player:getStateManager():changeState("idle")
+                    end
+                end
+            }
         }
     })
 
     player:getStateManager():changeState("idle")
 
-    block = entity.newStaticEntity(world, 200, 300, "block", 400, 100)
-    block2 = entity.newStaticEntity(world, 16, VIRTUAL_HEIGHT / 2, "block", 32, VIRTUAL_HEIGHT)
+    map = tilemap.newTilemap("src.scenes.map", assets.images.tileset)
 
+    local mapWidth, mapHeight = map:getMapSizeInPixels():getCoordinates()
+    blocks = {
+        entity.newStaticEntity(world, "wall", -7.5, VIRTUAL_HEIGHT / 2, 15, VIRTUAL_HEIGHT),
+        entity.newStaticEntity(world, "ground", mapWidth / 2, mapHeight - 47.5, mapWidth, 95)
+    }
 
-
-
-    -- map = tilemap.newTilemap("src.scenes.map", assets.images.tileset)
     -- cam = camera.newCamera(0, 0, 2)
 end
 
@@ -89,6 +91,7 @@ function love.update(dt)
     end
 
     world:update(dt)
+    player:update(dt)
 
     player:move(input.getAxis("left", "right", "leftx"), 0)
     if input.isActionPressed("jump") and (player:isOnFloor() or player:isByWall()) then
@@ -104,13 +107,9 @@ end
 function love.draw()
     love.graphics.scale(Scale, Scale)
 
+    -- cam:setCamera()
+    map:draw()
     player:draw()
     world:drawColliders()
-
-    -- cam:setCamera()
-
-    -- map:draw()
-    -- block:drawAll()
-
     -- cam:unsetCamera()
 end
